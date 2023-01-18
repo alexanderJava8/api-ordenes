f4220ed0befd3fea61bceaa297627d25b0c84d67
mi nombre es ricardo <spokbit.com>
2023-01-17 20:53:18 -0600

    add crud in ProductController

--- src/main/java/com/codmain/orderapi/controllers/ProductController.java
+++ src/main/java/com/codmain/orderapi/controllers/ProductController.java
@@ -2,13 +2,12 @@ package com.codmain.orderapi.controllers;
 
 import com.codmain.orderapi.entitys.Product;
 import com.codmain.orderapi.utils.Lists;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
@@ -33,4 +32,42 @@ public class ProductController {
                 .findAny()
                 .orElse(new Product(0L, "defecto"));
     }
+
+    @PostMapping(value = "/products/")
+    public Product create(@RequestBody Product product) {
+            Objects.requireNonNull(product, "product is null");
+
+            products.add(product);
+            return product;
+    }
+
+    @PutMapping(value = "/products/")
+    public Product updateProduct(@RequestBody Product product) {
+        Objects.requireNonNull(product, "product is null");
+        for (Product product1 : products) {
+            if (Objects.equals(product1.getId(), product.getId())) {
+                 product1.setName(product.getName());
+                 return product1;
+            }
+        }
+
+        throw new RuntimeException("no hay ese producto");
+    }
+
+    @DeleteMapping(value = "/products/{productId}")
+    public void deletedProducts(@PathVariable("productId") Long id) {
+        Product existProduct = products.stream()
+                .filter(product -> existProduct(product, id))
+                .findFirst().orElseThrow();
+
+        products.remove(existProduct);
+    }
+
+    private boolean existProduct(Product product, Long id) {
+            return Objects.equals(product.getId(), id);
+    }
+
+
+
+
 }
